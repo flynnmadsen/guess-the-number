36431a6075b14ad9cb0672609c3ced2d986e3de6
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-11-02 15:46:56 +0100

    A little stuck

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const message = document.getElementById("message")
-const guess = document.getElementById("user-input")
+const numberInput = document.getElementById("user-input")
 
 let secretNumber = randomInt(100)
+
+document.addEventListener("keyup" , function (event) {
+    if (event.key === "Enter") {
+        const guess = getUserInput
+
+      if (guess > secretNumber) {
+          setMessage("Too high!")
+      } else if (guess == secretNumber) {
+          setMessage("You are correct!")
+      } else {
+          setMessage("Too low!")
+      } {
+          
+      }
+    }
+})
 
 function randomInt() {
     return Math.floor(Math.random()* 101)
@@ -9,6 +25,9 @@ function randomInt() {
 console.log(secretNumber)
 
 function getUserInput() {
-    return parseInt(guess.value)
+    return parseInt(numberInput.value)
 }
 
+function setMessage(msg) {
+    message.innerHTML = msg
+}
