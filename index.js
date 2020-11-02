9cc89d8e0879790022eb2e87878b3ba5c195a6f7
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-11-02 14:15:07 +0100

    Forward march!

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const secretNumber = (RandomInt)
+
 function RandomInt()
 {
     return Math.floor(Math.random() * 101)
 
 }
 console.log(RandomInt())
+
+function getUserGuess() {
+    const stringValue = document.getElementById("user-input").value
+    return parseInt(stringValue, 10)
+
+}
+
+document.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        const guess = getUserGuess()
+        console.log(guess)
+    }
+})
 
